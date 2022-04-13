0046f234f4d0fc87e5ec2bfc78b1c92411a5f36c
Andrea Labra Orozco <andrealabra21@gmail.com>
2022-04-13 11:25:09 -0700

    Initial commit plus log in screen

---
+++ app/src/main/java/com/example/project3/Login.java
@@ -0,0 +1,25 @@
+package com.example.project3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText phone = findViewById(R.id.phone);
+        final EditText password = findViewById(R.id.pasword);
+        final Button loginBtn = findViewById(R.id.loginBtn);
+        final TextView registerNowBtn = findViewById(R.id.registerNowBtn);
+
+
+
+    }
+}
