f3ada20843e9b61e2e24c5be8c75cb759bdfada5
Andrea Labra Orozco <andrealabra21@gmail.com>
2022-04-16 14:06:36 -0700

    Sucessfully created LogIn, Registration and App is connected to firebase

--- app/src/main/java/com/example/project3/Login.java
+++ app/src/main/java/com/example/project3/Login.java
@@ -1,13 +1,26 @@
 package com.example.project3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Login extends AppCompatActivity {
+
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://helpme-19a66-default-rtdb.firebaseio.com/");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,61 @@ public class Login extends AppCompatActivity {
         final EditText password = findViewById(R.id.pasword);
         final Button loginBtn = findViewById(R.id.loginBtn);
         final TextView registerNowBtn = findViewById(R.id.registerNowBtn);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String phoneTxt = phone.getText().toString();
+                final String passwordTxt = password.getText().toString();
+
+                if(phoneTxt.isEmpty() || passwordTxt.isEmpty()){
+                    Toast.makeText(Login.this,"Please enter phone and password", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            //check if the phone number is in firebase
+                            if(snapshot.hasChild(phoneTxt)){
+                                //yes phone is in firebase, now check the password
+                                final String getPassword = snapshot.child(phoneTxt).child("password").getValue(String.class);
+
+                                if(getPassword.equals(passwordTxt)){
+                                    Toast.makeText(Login.this, "You have sucessfully logged in", Toast.LENGTH_SHORT).show();
+
+                                    //This is where we will go from login screens to main
+                                    startActivity(new Intent(Login.this, MainActivity.class));
+                                    finish();
+                                }
+                                else{
+                                    Toast.makeText(Login.this, "Wrong Password", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                            else{
+                                Toast.makeText(Login.this, "Wrong Phone", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+            }
+        });
+
+        registerNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                //this will open registration activity
+                startActivity(new Intent(Login.this, Register.class));
+
+            }
+        });
 
 
 
--- app/src/main/java/com/example/project3/Register.java
+++ app/src/main/java/com/example/project3/Register.java
@@ -1,14 +1,99 @@
 package com.example.project3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Register extends AppCompatActivity {
 
+    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://helpme-19a66-default-rtdb.firebaseio.com/");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final EditText fullname = findViewById(R.id.fullname);
+        final EditText email = findViewById(R.id.email);
+        final EditText phone = findViewById(R.id.phone);
+        final EditText password = findViewById(R.id.pasword);
+        final EditText conPassword = findViewById(R.id.conPasword);
+
+        final Button registerBtn = findViewById(R.id.registerBtn);
+        final TextView loginNowBtn = findViewById(R.id.loginNow);
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //turn data into string val
+                final String fullnameTxt = fullname.getText().toString();
+                final String emailTxt = email.getText().toString();
+                final String phoneTxt = phone.getText().toString();
+                final String passwordTxt = password.getText().toString();
+                final String conPasswordTxt = conPassword.getText().toString();
+
+                //make sure all spaces are fielded
+                if(fullnameTxt.isEmpty() || emailTxt.isEmpty() || phoneTxt.isEmpty()
+                || passwordTxt.isEmpty())
+                {
+                    Toast.makeText(Register.this, "Fill in all fields to continue", Toast.LENGTH_SHORT).show();
+                }
+
+                //This is to see if all passwords match
+                else if(!passwordTxt.equals(conPasswordTxt))
+                {
+                    Toast.makeText(Register.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                }
+
+                else{
+                    databaseReference.child("users").addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            //see if the number is registered
+
+                            if(snapshot.hasChild(phoneTxt)){
+                                Toast.makeText(Register.this, "This phone number is already associated with an account", Toast.LENGTH_SHORT).show();
+                            }
+                            else{
+                                //send data to firebase & making phone number the unique id, everything wil be under phone number
+                                databaseReference.child("users").child(phoneTxt).child("fullname").setValue(fullnameTxt);
+                                databaseReference.child("users").child(phoneTxt).child("email").setValue(emailTxt);
+                                databaseReference.child("users").child(phoneTxt).child("password").setValue(passwordTxt);
+
+                                Toast.makeText(Register.this, "Registration done sucessfully", Toast.LENGTH_SHORT).show();
+                                finish();
+
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                }
+
+            }
+        });
+
+        loginNowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 }
