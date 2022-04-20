9a7db801d8907e9aa3574605b294cf18e4078547
Andrea Labra Orozco <andrealabra21@gmail.com>
2022-04-20 10:11:20 -0700

    Buttons on main page, for other screens

--- app/src/main/java/com/example/project3/MainActivity.java
+++ app/src/main/java/com/example/project3/MainActivity.java
@@ -2,15 +2,48 @@ package com.example.project3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    private Button move;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //connect buttons to activity pages
         setContentView(R.layout.activity_main);
+        move = findViewById(R.id.postitBtn);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,PostingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        move = findViewById(R.id.youtubeBtn);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,YouTubeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        move = findViewById(R.id.calculateitBt);
+        move.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,FinalCalculator.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
