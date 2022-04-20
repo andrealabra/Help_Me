727d83ea733937c98a23619c1ee2790c9317edb1
Andrea Labra Orozco <andrealabra21@gmail.com>
2022-04-19 22:04:31 -0700

    Calculator updates

---
+++ app/src/main/java/com/example/project3/FinalCalculator.java
@@ -0,0 +1,18 @@
+package com.example.project3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.project3.databinding.ActivityFinalCalculatorBinding;
+
+public class FinalCalculator extends AppCompatActivity {
+
+   //  private lateinit var binding : ActivityFinalCalculatorBinding
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_calculator);
+    }
+}
--- app/src/main/java/com/example/project3/YouTubeActivity.java
+++ app/src/main/java/com/example/project3/YouTubeActivity.java
@@ -1,7 +1,5 @@
 package com.example.project3;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.Toast;
 
