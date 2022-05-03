a3c6e13e50ee31a510febc9f637d500314c7436c
Adrian Anderson <74568128+andersonadrian@users.noreply.github.com>
2022-05-03 09:19:47 -0700

    Updated the MainActivity.java file with the proper comments

--- app/src/main/java/com/example/project3/MainActivity.java
+++ app/src/main/java/com/example/project3/MainActivity.java
@@ -10,9 +10,24 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Button variable to launch the Post Activity
+     */
     private Button movePost;
+
+    /**
+     * Button variable to launch the Youtube Activity
+     */
     private Button moveYoutube;
+
+    /**
+     * Button variable to launch the Calculator Activity
+     */
     private Button moveCalculator;
+
+    /**
+     * Button variable to Logout of the application
+     */
     private Button moveLogout;
 
     @Override
@@ -25,8 +40,15 @@ public class MainActivity extends AppCompatActivity {
         moveCalculator = findViewById(R.id.calculateitBtn);
         moveLogout = findViewById(R.id.logoutBtn);
 
+        /**
+         * Represent the Id for the user to able to pass data back to firebase.
+         */
         String phoneId = getIntent().getStringExtra("phoneId");
 
+        /**
+         * movePost Button to listen to the receive click event.
+         * @Override onClick event handler to launch the Post screen
+         */
         movePost.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -36,6 +58,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /**
+         * moveYoutube Button to listen to the receive click event.
+         * @Override onClick event handler to launch the Youtube screen
+         */
         moveYoutube.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,6 +69,11 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        /**
+         * moveCalculator Button to listen to the receive click event.
+         * @Override onClick event handler to launch the Calculator screen
+         */
 
         moveCalculator.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,6 +83,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        /**
+         * moveLogout Button to listen to the receive click event.
+         * @Override onClick event handler to log the user out of the app.
+         */
         moveLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
